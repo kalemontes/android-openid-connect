e2a69ba8f2b54c9bef95b5bca24d0d6aafe44e9f
Camilo Montes <ale8689@gmail.com>
2015-06-11 15:14:58 +0200

    Changes the APIUtility to request using the accessToken instead of the idToken. Maybe this is just OpenAm specific but reading OIDC doc doesnt looks like...

--- app/src/main/java/com/lnikkila/oidcsample/APIUtility.java
+++ app/src/main/java/com/lnikkila/oidcsample/APIUtility.java
@@ -14,11 +14,12 @@ import com.lnikkila.oidcsample.oidc.authenticator.Authenticator;
 import java.io.IOException;
 import java.util.Map;
 
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
 import static java.net.HttpURLConnection.HTTP_FORBIDDEN;
 import static java.net.HttpURLConnection.HTTP_UNAUTHORIZED;
 
 /**
- * An incomplete class that illustrates how to make API requests with the ID Token.
+ * An incomplete class that illustrates how to make API requests with the Access Token.
  *
  * @author Leo Nikkilä
  */
@@ -50,34 +51,38 @@ public class APIUtility {
                                      boolean doRetry) throws IOException {
 
         AccountManager accountManager = AccountManager.get(context);
-        String idToken;
+		String accessToken;
 
-        // Try retrieving an ID token from the account manager. The boolean true in the invocation
+        // Try retrieving an access token from the account manager. The boolean true in the invocation
         // tells Android to show a notification if the token can't be retrieved. When the
         // notification is selected, it will launch the intent for re-authorisation. You could
         // launch it automatically here if you wanted to by grabbing the intent from the bundle.
         try {
-            AccountManagerFuture<Bundle> futureManager = accountManager.getAuthToken(account,
-                    Authenticator.TOKEN_TYPE_ID, null, true, null, null);
 
-            idToken = futureManager.getResult().getString(AccountManager.KEY_AUTHTOKEN);
+            AccountManagerFuture<Bundle> futureManager = accountManager.getAuthToken(account,
+                    Authenticator.TOKEN_TYPE_ACCESS, null, true, null, null);
+            accessToken = futureManager.getResult().getString(AccountManager.KEY_AUTHTOKEN);
         } catch (Exception e) {
-            throw new IOException("Could not get ID token from account.", e);
+            throw new IOException("Could not get access token from account.", e);
         }
 
-        // Prepare an API request using the token
+        // Prepare an API request using the accessToken
         HttpRequest request = new HttpRequest(url, method);
-        request = OIDCUtils.prepareApiRequest(request, idToken);
+        request = OIDCUtils.prepareApiRequest(request, accessToken);
 
         if (request.ok()) {
             return request.body();
         } else {
             int code = request.code();
 
-            if (doRetry && (code == HTTP_UNAUTHORIZED || code == HTTP_FORBIDDEN)) {
+            if (doRetry && (code == HTTP_UNAUTHORIZED || code == HTTP_FORBIDDEN || code == HTTP_BAD_REQUEST)) {
                 // We're being denied access on the first try, let's renew the token and retry
                 String accountType = context.getString(R.string.ACCOUNT_TYPE);
-                accountManager.invalidateAuthToken(accountType, idToken);
+
+                accountManager.setAuthToken(account, Authenticator.TOKEN_TYPE_ID, null);
+                accountManager.setAuthToken(account, Authenticator.TOKEN_TYPE_REFRESH, null);
+
+                accountManager.invalidateAuthToken(accountType, accessToken);
 
                 return makeRequest(context, method, url, account, false);
             } else {
